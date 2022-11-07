7114086a2aecfcab385e5a7bb735a8173002bc94
Martin Ro <martin-ro@users.noreply.github.com>
2022-11-08 06:38:42 +0800

    wip

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use ZepFietje\FilamentDateTimeSlotPicker\DateTimeSlotPicker;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id'),
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+
+                Tables\Actions\Action::make('book')
+                    ->form([
+                        DateTimeSlotPicker::make('lesson')
+                            ->options(function (Model $record) {
+                                return [
+                                    [
+                                        now()->startOfDay()->addDays($record->id),
+                                        now()->startOfDay()->addDays($record->id)->addMinutes(30),
+                                        fake()->uuid,
+                                    ]
+                                ];
+                            })
+                    ])
+                    ->action(fn(array $data) => ray($data))
+                    ->requiresConfirmation(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
